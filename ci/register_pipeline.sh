3dd4fc9bff9bc2e1e7f530948d6da7f9a1d44d91
Joshua McKenty <jmckenty@pivotal.io>
2016-01-22 14:03:38 -0800

    Main content

--- ci/register_pipeline.sh
+++ ci/register_pipeline.sh
@@ -1,1 +1,1 @@
-fly --target dragon configure --vars-from credentials.yml --config pws-push.yml industryday-website
+fly --target dragon set-pipeline --var "github-private-key=$(cat ~/.ssh/id_rsa)" --load-vars-from credentials.yml --config pws-push.yml --pipeline industryday-website
